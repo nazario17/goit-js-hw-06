44d53ecdb75e4ac8ec11372353e7d01b8649f002
nazario17 <hazarkorchak@gmail.com>
2023-03-29 19:44:18 +0300

    fix task 1

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const categoriesList = document.querySelectorAll('.item')
 console.log("Number of categories:", categoriesList.length);
 
 categoriesList.forEach(item => {
-    const categoryName = item.querySelectorAll('h2').textContent 
+    const categoryName = item.querySelector('h2').textContent 
     const categoryElements = item.querySelectorAll('li').length
     console.log(`Category: ${categoryName}\nElements: ${categoryElements}`);
 }); 
